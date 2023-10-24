e7ddf5306eb8e6e6d6d1dbdcf06c589cb01a011d
scolp <scolpig@gmail.com>
2023-10-24 11:55:42 +0900

    one sentence

---
+++ job04_one_sentence.py
@@ -0,0 +1,22 @@
+import pandas as pd
+
+df = pd.read_csv('./crawling_data/cleaned_review.csv')
+df.dropna(inplace=True)
+df.info()
+print(df.head(10))
+
+one_sentences = []
+for title in df['title'].unique():
+    temp = df[df['title'] == title]
+    one_sentence = ' '.join(temp['cleaned_sentences'])
+    one_sentences.append(one_sentence)
+df_one = pd.DataFrame({'titles':df['title'].unique(), 'reviews':one_sentences})
+print(df_one.head())
+df_one.info()
+df_one.to_csv('./crawling_data/cleaned_one_review.csv', index=False)
+
+
+
+
+
+
